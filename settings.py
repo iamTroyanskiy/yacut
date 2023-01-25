4a42c2e521f924ad42220d6528963bfbab25b4ff
Anton Yaitskiy <tonik350@yandex.ru>
2023-01-26 00:53:18 +0700

    First commit

--- settings.py
+++ settings.py
@@ -5,6 +5,9 @@ MAX_URL_SIZE = 6
 
 class Config(object):
     JSON_AS_ASCII = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URI',
+        default='sqlite:///db.sqlite3'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
